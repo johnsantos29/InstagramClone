6f7ffdd075c501076034cbd4efb97081a6ac74f7
John Erick Santos <johnsantosdev@gmail.com>
2023-06-18 23:07:02 +1000

    todo: HomeViewController - present by default and then ask to login if not authenticated

--- InstagramClone/Controllers/Other/Onboarding/RegistrationViewController.swift
+++ InstagramClone/Controllers/Other/Onboarding/RegistrationViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 final class RegistrationViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        
     }
 }
