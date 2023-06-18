0ffea408ef71319a5fb48c0da1a47d67dc00e708
John Erick Santos <johnsantosdev@gmail.com>
2023-06-18 21:36:23 +1000

    Initial Commit

---
+++ InstagramClone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by John Erick Santos on 18/6/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
