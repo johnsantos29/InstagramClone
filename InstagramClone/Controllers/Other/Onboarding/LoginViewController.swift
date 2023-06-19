75c800db495c9575e4cec32823504a61774a723b
John Erick Santos <johnsantosdev@gmail.com>
2023-06-20 05:41:45 +1000

    feat: Username and Password textfield, header view add bg image.
    todo: Continue on to header view

--- InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -8,23 +8,56 @@
 import UIKit
 
 final class LoginViewController: UIViewController {
-    private let usernameEmailField: UITextField = .init()
+    enum Constants {
+        static let cornerRadius: CGFloat = 8.0
+    }
+    
+    private let usernameEmailField: UITextField = {
+        let field = UITextField()
+        field.placeholder = "Username or Email"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
+        
+        return field
+    }()
     
     private let passwordField: UITextField = {
         let field = UITextField()
         field.isSecureTextEntry = true
+        field.placeholder = "Password"
+        field.returnKeyType = .next
+        field.leftViewMode = .always
+        field.leftView = UIView(frame: CGRect(x: 0, y: 0, width: 10, height: 0))
+        field.autocapitalizationType = .none
+        field.autocorrectionType = .no
+        field.layer.masksToBounds = true
+        field.layer.cornerRadius = Constants.cornerRadius
+        field.backgroundColor = .secondarySystemBackground
         
         return field
     }()
     
     private let loginButton: UIButton = {
         let button = UIButton()
+        button.setTitle("Log In", for: .normal)
+        button.layer.masksToBounds = true
+        button.layer.cornerRadius = Constants.cornerRadius
+        button.backgroundColor = .systemBlue
+        button.setTitleColor(.white, for: .normal)
         
         return button
     }()
     
     private let createAccountButton: UIButton = {
         let button = UIButton()
+        button.setTitle("New User? Create an Account", for: .normal)
+        button.setTitleColor(.label, for: .normal)
         
         return button
     }()
@@ -42,7 +75,11 @@ final class LoginViewController: UIViewController {
     }()
     
     private let headerView: UIView = {
+        let bgImageView = UIImageView(image: UIImage(named: "gradient"))
+        
         let uiHeaderView = UIView()
+        uiHeaderView.clipsToBounds = true
+        uiHeaderView.addSubview(bgImageView)
         
         return uiHeaderView
     }()
@@ -71,8 +108,22 @@ final class LoginViewController: UIViewController {
             x: 0,
             y: view.safeAreaInsets.top,
             width: view.width,
-            height: 200
+            height: view.height / 3
         )
+        
+        configureHeaderView()
+    }
+    
+    private func configureHeaderView() {
+        guard headerView.subviews.count == 1 else {
+            return
+        }
+        
+        guard var bgImageView = headerView.subviews.first else {
+            return
+        }
+        
+        bgImageView.frame = headerView.bounds
     }
     
     @objc private func didTapLoginButton() {}
