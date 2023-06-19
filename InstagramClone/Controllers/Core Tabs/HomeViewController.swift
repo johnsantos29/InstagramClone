e1406a5d72011b550ddb6c0ba93d71a03a558e99
John Erick Santos <johnsantosdev@gmail.com>
2023-06-19 20:57:15 +1000

    feat: init HomeViewController viewDidAppear - not authenticated case

--- InstagramClone/Controllers/Core Tabs/HomeViewController.swift
+++ InstagramClone/Controllers/Core Tabs/HomeViewController.swift
@@ -5,11 +5,30 @@
 //  Created by John Erick Santos on 18/6/2023.
 //
 
+import FirebaseAuth
 import UIKit
 
 final class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+    }
+
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        handleNotAuthenticated()
+    }
+
+    private func handleNotAuthenticated() {
+        // Check auth status
+        if Auth.auth().currentUser == nil {
+            // Show login
+            let loginVC = LoginViewController()
+
+            // full screen for the user to not be able to close the modal
+            loginVC.modalPresentationStyle = .fullScreen
+            present(loginVC, animated: false)
+        }
     }
 }
