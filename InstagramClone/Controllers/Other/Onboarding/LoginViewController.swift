c2ddd1e92b0c732677d93c5ba7b866c20952aa66
John Erick Santos <johnsantosdev@gmail.com>
2023-06-19 21:01:44 +1000

    feat: New group from selection for the general view controllers, set dummy bg color for LoginVC

--- InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
+++ InstagramClone/Controllers/Other/Onboarding/LoginViewController.swift
@@ -11,6 +11,6 @@ final class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemMint
     }
 }
