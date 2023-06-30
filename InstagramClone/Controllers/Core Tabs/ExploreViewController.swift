b62dd95b8a6099831a3fadcde048bc7141ce6577
John Erick Santos <johnsantosdev@gmail.com>
2023-06-30 09:40:46 +0900

    feat: Stub out ExploreViewController - collection view and search bar

--- InstagramClone/Controllers/Core Tabs/ExploreViewController.swift
+++ InstagramClone/Controllers/Core Tabs/ExploreViewController.swift
@@ -8,8 +8,45 @@
 import UIKit
 
 final class ExploreViewController: UIViewController {
+    private let searchBar: UISearchBar = {
+        let searchBar = UISearchBar()
+        searchBar.backgroundColor = .secondarySystemBackground
+        
+        return searchBar
+    }()
+    
+    private var collectionView: UICollectionView?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        // search bar
+        navigationController?.navigationBar.topItem?.titleView = searchBar
+        
+        // collection view
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.sectionInset = UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
+        collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        guard let collectionView = collectionView else {
+            return
+        }
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        view.addSubviews(collectionView)
+    }
+}
+
+extension ExploreViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        return UICollectionViewCell()
     }
 }
