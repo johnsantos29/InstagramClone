686640150258d0101415cdbe10dedb95965bfd97
John Erick Santos <johnsantosdev@gmail.com>
2023-06-30 09:26:31 +0900

    feat: stub out CameraViewController

--- InstagramClone/Controllers/Core Tabs/CameraViewController.swift
+++ InstagramClone/Controllers/Core Tabs/CameraViewController.swift
@@ -5,11 +5,16 @@
 //  Created by John Erick Santos on 18/6/2023.
 //
 
+import AVFoundation
 import UIKit
 
 final class CameraViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        
+        navigationController?.setNavigationBarHidden(true, animated: false)
     }
+    
+    private func didTapTakePicture() {}
 }
