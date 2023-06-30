3119ba38e4844ca0f4ae7bd0730a9bb8b8bd6cad
John Erick Santos <johnsantosdev@gmail.com>
2023-06-30 09:23:32 +0900

    feat: init table view in NotificationsViewController

--- InstagramClone/Controllers/Core Tabs/NotificationsViewController.swift
+++ InstagramClone/Controllers/Core Tabs/NotificationsViewController.swift
@@ -8,8 +8,39 @@
 import UIKit
 
 final class NotificationsViewController: UIViewController {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "Notifications"
         view.backgroundColor = .systemBackground
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+        view.addSubviews(tableView)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        tableView.frame = view.bounds
+    }
+}
+
+extension NotificationsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        
+        return cell
     }
 }
